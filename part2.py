0d522502841406f440005664a053ad34c25b6981
ellias palcu <epalcu@vols.utk.edu>
2017-03-16 20:15:33 -0400

    Project Complete!
    - part2.py fully commented
    - Usage document done
    - Zip file for submission generated

--- part2.py
+++ part2.py
@@ -9,6 +9,7 @@ hashString = "1FnK0Fv7JOHZgh82rtmWZDW9Y9KQdgz9Gh6E6Cs35AY="
 hashSecret = "R5SOwt7UwrKo0sBXxDhSNXeCpa7Dffl0pHHG0bHIrUY="
 dictionary = []
 
+# Read in dictionary into a list
 def open_dictionary(d_list, file):
     with open(file) as fname:
         lines = fname.readlines()
@@ -21,6 +22,9 @@ def open_dictionary(d_list, file):
 if __name__ == "__main__":
     dictionary = []
     dictionary = open_dictionary(dictionary, sys.argv[1])
+
+    # Cycle through each dictionary word until the hash of the concatenated string
+    # is equal to the initally received hashString
     for i in dictionary:
         string = userName + ":" + challengeString + ":" + i
         h = hashlib.sha256(string).digest()
